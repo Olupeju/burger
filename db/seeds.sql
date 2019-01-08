07ca571461f7b09eb2184cb8d41e035aadd1b6ab
unknown <olayika@hotmail.com>
2019-01-07 23:32:49 -0600

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Rodeo Burger');
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Toro Burger');
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Brisket Burger');
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Texas Burger');
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Buffalo Burger');
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('BBQ Chicken Burger');
