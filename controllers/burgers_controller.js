54389345425c5ab5aa41516fe50e1b25791eece8
unknown <olayika@hotmail.com>
2019-01-09 18:38:52 -0600

    fixed error

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,31 +4,22 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
-// set route for home page
+// set route 
 router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
+  burger.all(function(data) {
   	//set table data as an array with key burger
     var hbsObject = {
       burger: data
     };
     console.log(hbsObject);
-    //render data into index.handlebars
     res.render("index", hbsObject);
   });
 });
 
-
-//set up api view in case you want to view all your data
-// router.get("/api", function(req, res){
-// 	burger.all(function(data){
-// 		res.json(data);
-// 	})
-// });
-
 //post new burger to the table
 router.post("/", function(req, res){
 	console.log(req.body.input);
-	burger.insertOne(req.body.input, function(){
+	burger.create(req.body.input, function(){
 		res.redirect("/");
 	})
 });
@@ -39,7 +30,7 @@ router.put("/:id", function(req, res) {
 
   console.log(id);
 
-  burger.updateOne("devoured",true, id, function(){
+  burger.update("devoured",true, id, function(){
   	res.redirect("/");
   })
 });
